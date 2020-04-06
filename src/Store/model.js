4002e089d805aee5f1f2a0e429cb05dbf90dfce5
Thagana <samuelmthwa79@gmail.com>
2020-04-07 00:59:10 +0200

    :truck: Store

--- src/Store/model.js
+++ src/Store/model.js
@@ -1,12 +1,32 @@
-import { action } from 'easy-peasy';
+import { action, thunk } from 'easy-peasy';
+import AsyncStorage from '@react-native-community/async-storage';
 
 export default {
     /** STATE */
     isLoggedin: false,
-
-    /**ACTIONS */
+    /** ACTIONS */
+    signInhandle: thunk(async (actions, payload) => {
+        try{
+            await AsyncStorage.setItem("IS_LOGGED_IN", true);
+            actions.loggin();
+        }catch(error){
+            console.log(error)
+        }
+    }),
+    signOutHandle: thunk(async(action, payload) => {
+    try {
+        await AsyncStorage.removeItem("IS_LOGGED_IN")
+        actions.signOut()
+    } catch (error) {
+        console.log(error)
+    }
+    }),
     loggin: action((state, payload) => {
         state.isLoggedin = true;
+
+    }),
+    signOut: action((state, payload) => {
+        state.isLoggedin = false;
     })
 }
 
